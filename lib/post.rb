a1a4e2f31a0228ed33df8854b0a205b2be1c645e
Samantha Aleman <samantha.aleman25@gmail.com>
2020-08-10 17:50:35 +0000

    Automatically backed up by Learn

---
+++ lib/post.rb
@@ -0,0 +1,30 @@
+class Post 
+  
+attr_accessor :name , :author, :title
+  
+@@all = [] 
+  
+def initialize(name) 
+  @name = name 
+  @@all
+  save
+end 
+  
+def title 
+  @title = title
+end 
+
+
+def save 
+  @@all << self 
+end 
+
+def self.all 
+  @@all 
+end 
+
+def author_name
+  self.author.name
+end 
+  
+end 

7eae3a1d71daa1ecb6d2a775bc9d9572b4d29032
Samantha Aleman <samantha.aleman25@gmail.com>
2020-08-10 17:58:22 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,25 @@
+class Post 
+  
+attr_accessor :title , :author
+  
+@@all = [] 
+  
+def initialize(title) 
+  @title = title 
+  @@all
+  save
+end 
+  
+def save 
+  @@all << self 
+end 
+
+def self.all 
+  @@all 
+end 
+
+def author_name
+  self.author.name if author
+end 
+  
+end 
